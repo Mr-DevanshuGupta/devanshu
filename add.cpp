3fdbd1eb84c4f64f07d1bdbf0566a9d36331fc11
Devanshu Gupta <devanshu.kissu.0000@gmail.com>
2022-09-27 15:44:16 +0530

    Added a dev and add file in cpp

---
+++ add.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 23;
+    int b = 34;
+    cout<<"Adding of this two numbers is "<< a+b<<endl;
+    return 0;
+    
+}
---
+++ dev.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"I am the best in the world";
+    return 0;
+}
